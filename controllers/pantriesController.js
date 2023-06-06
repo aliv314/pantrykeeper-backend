8d9208f6d6c34e4b245b5047a48cc6cbb9a051ca
alv-314 <alicov314@gmail.com>
2023-06-06 13:48:58 -0400

    Moved getPantries to pantry operations

--- controllers/pantriesController.js
+++ controllers/pantriesController.js
@@ -1,5 +1,26 @@
 const {db} = require('../firebase');
 
+//REQ: id
+//RES: [{PANTRYOBJ}]
+exports.getPantries = async(req, res) => {
+    try{
+        const pantriesRef = await db.collection('pantries').where('owner_id', '==', req.params.id);
+        const pantriesSnapshot = await pantriesRef.get();
+        const pantriesData = []
+        await Promise.all(pantriesSnapshot.docs.map( (pantry) => {
+            pantriesData.push({pantry_id: pantry.id,
+                ...pantry.data()});
+        })).catch(error => {
+            console.log(error);
+        })
+
+        res.status(200).json(pantriesData);
+    }catch(error){
+        console.log(error)
+        res.status(404).send("Error getting pantries.")
+    }
+}   
+
 
 function newPantry (owner_id, owner_name, pantry_name, pantry_img, num_foods, num_dishes, num_ingredients){
     return{
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,27 +6,6 @@ function newUser(friendCode) {
     }
 }
 
-//REQ: id
-//RES: [{PANTRYOBJ}]
-exports.getUserPantries = async(req, res) => {
-    try{
-        const pantriesRef = await db.collection('pantries').where('owner_id', '==', req.params.id);
-        const pantriesSnapshot = await pantriesRef.get();
-        if(pantriesSnapshot.exists){
-            return res.status(200).json([]);
-        }
-        let pantries = []
-        pantriesSnapshot.forEach( (pantry) => {
-            pantries.push({pantry_id: pantry.id,
-                ...pantry.data()});
-        });
-        res.status(200).json(pantries);
-    }catch(error){
-        console.log(error)
-        res.status(404).send("Error getting pantries.")
-    }
-}
-
 //Posts user Friend code to 
 exports.postUser = async (req, res) => {
     try{
